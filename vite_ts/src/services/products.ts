563ef4b2b1d592c7d423ddda69f577fee86eec43
Shane Ziegler <56492231+shaneziegler@users.noreply.github.com>
2024-09-28 17:08:02 -0500

    Adding zod api validation checking

--- vite_ts/src/services/products.ts
+++ vite_ts/src/services/products.ts
@@ -1,37 +1,33 @@
 import axios from "axios";
 import { CartAdd, ProductEntry, ProductId, ProductListing } from "../types";
-// import { z } from "zod";
+import { z } from "zod";
 
-// const productSchema = z.object({
-//   _id: z.string(),
-//   title: z.string(),
-//   price: z.number(),
-//   quantity: z.number(),
-//   createdAt: z.string(),
-//   updatedAt: z.string(),
-//   __v: z.number()
-// });
+const productSchema = z.object({
+  _id: z.string(),
+  title: z.string(),
+  price: z.number(),
+  quantity: z.number()
+});
 
-
+const getProductsResponseSchema = z.array(productSchema);
 
 export const getProducts = async () => {
   const { data } = await axios.get<ProductListing[]>("/api/products");
-  return data;
+  return getProductsResponseSchema.parse(data);
 };
 
 export const createProduct = async (newProduct: ProductEntry) => {
   const { data } = await axios.post<ProductListing>("/api/products", newProduct);
-  return data;
+  return productSchema.parse(data);
 };
 
 export const deleteProduct = async (id: ProductId) => {
   await axios.delete<void>(`/api/products/${id}`);
-  // return data;
 };
 
 export const updateProduct = async (id: ProductId, product: ProductEntry) => {
   const { data } = await axios.put<ProductListing>(`/api/products/${id}`, product);
-  return data;
+  return productSchema.parse(data);
 };
 
 export const addCartItem = async (id: ProductId) => {
@@ -41,10 +37,9 @@ export const addCartItem = async (id: ProductId) => {
 
 export const getCartItems = async () => {
   const { data } = await axios.get<ProductListing[]>(`/api/cart`);
-  return data;
+  return getProductsResponseSchema.parse(data);
 };
 
 export const checkoutItems = async () => {
   await axios.post<void>("/api/checkout");
-  // return data;
 };
